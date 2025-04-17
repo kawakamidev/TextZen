377192743d34621df7ea703f8dae535f5c0f1568
KAWAKAMI Moeki <hi@moeki.org>
2025-04-17 21:23:17 +0900

    Fix navigating with internal link

--- src/main/listeners/handle-file-navigate.ts
+++ src/main/listeners/handle-file-navigate.ts
@@ -1,5 +1,13 @@
+import { existsSync } from 'node:fs'
 import { mainWindow } from '..'
+import { handleFileCreate } from './handle-file-create'
+import { store } from '../lib/store'
+import { join } from 'node:path'
 
-export const handleFileNavigate = (_: Electron.IpcMainInvokeEvent, title: string): void => {
+export const handleFileNavigate = async (_: Electron.IpcMainInvokeEvent, title: string): void => {
+  const dirPath = store.get('general.path') as string
+  if (!existsSync(join(dirPath, `${title}.md`))) {
+    await handleFileCreate(null, title, '')
+  }
   mainWindow?.webContents.send('replace', title)
 }
