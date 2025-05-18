26a4c0ac79dad0c8d9d5bcf8c563628b2ab6ccde
KAWAKAMI Moeki <hi@moeki.org>
2025-05-18 14:47:35 +0900

    Update style

--- src/renderer/src/lib/highlights.ts
+++ src/renderer/src/lib/highlights.ts
@@ -6,15 +6,15 @@ const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
 
 let specs = [
   { tag: tags.meta, color: isDark ? '#b3b5b3' : '#404740' },
-  { tag: tags.link, textDecoration: 'underline' },
-  { tag: tags.heading, textDecoration: 'underline', fontWeight: 'bold' },
+  { tag: tags.link },
+  { tag: tags.heading, color: '#0754a6', fontWeight: 'bold' },
   { tag: tags.emphasis, fontStyle: 'italic' },
   { tag: tags.strong, fontWeight: 'bold' },
   { tag: tags.strikethrough, textDecoration: 'line-through' },
   { tag: tags.keyword, color: isDark ? '#c678dd' : '#708' },
   {
     tag: [tags.atom, tags.bool, tags.url, tags.contentSeparator, tags.labelName],
-    color: isDark ? '#61afef' : '#219'
+    color: isDark ? '#61afef' : 'rgba(20, 20, 20)'
   },
   { tag: [tags.literal, tags.inserted], color: isDark ? '#98c379' : '#164' },
   { tag: [tags.string, tags.deleted], color: isDark ? '#e06c75' : '#a11' },
